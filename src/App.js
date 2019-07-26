d5455920cbecf21758f2edc7d0226664ae02ba01
vitao80 <victor@keybest.com.br>
2019-07-26 06:01:27 -0300

    Spinner component. env local

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
 
-class App extends React.Component{  
-
+class App extends React.Component {
   state = {
     users: [],
     loading: false
-  }
+  };
 
   async componentDidMount() {
     //Não é correto em React
     //this.state.loading = true;
     //Correto
     this.setState({ loading: true });
-
-    const res = await axios.get('https://api.github.com/users');
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+    const res = await axios.get(`https://api.github.com/users?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     console.log(res.data);
-    this.setState({ users: res.data, loading: true });
+    this.setState({ users: res.data, loading: false });
   }
 
-  render(){    
-  return (
-    <div className="App">
-      <Navbar title="Github Finder" icon="fab fa-github" />
-      <div className="container">
-        <Users loading={this.state.loading} users={this.state.users} />
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-      
-    </div>
-  );
+    );
   }
 }
 
